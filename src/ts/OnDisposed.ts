9f8db8c84bb78bf0fab0093f4db6667ffeaad8a6
Michael MacFadden <michael@convergencelabs.com>
2019-11-29 17:36:41 -0800

    Updated copyright on the code and the LICENSE.

--- src/ts/OnDisposed.ts
+++ src/ts/OnDisposed.ts
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2019 Convergence Labs, Inc.
+ *
+ * This file is part of the CodeMirror Collaborative Extensions, which is
+ * released under the terms of the MIT license. A copy of the MIT license
+ * is usually provided as part of this source code package in the LICENCE
+ * file. If it was not, please see <https://opensource.org/licenses/MIT>
+ */
+
 /**
  * A simple callback type that signifies a resource has been disposed.
  *
